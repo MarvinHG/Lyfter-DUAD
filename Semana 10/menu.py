a12f8783bd6199693e1019ed10a9c6bd0778111f
Marvin Hernandez G <marvin.ahg@gmail.com>
2026-02-25 17:57:45 -0600

    Refactor project structure. Improved modularity, unified student comparison logic, and fixed import errors.

--- Semana 10/menu.py
+++ Semana 10/menu.py
@@ -1,139 +1,58 @@
 # This module contains the main menu functions for the student management system.
-from actions import is_valid_name, is_valid_section, student_exists, is_valid_grade, sort_students_by_average, get_student, delete_student, get_failed_students
+from actions import get_basic_student_info, get_student_grades, get_all_students_formatted, calculate_average, calculate_general_average, get_top_3_students_formatted, get_student, get_failed_students_formatted, delete_student
 from data import imported_students_from_csv , export_students_to_csv
 
 
 # ========== Function to fill student information ========== 
 def menu_1_fill_student_info(students):
-    # Loop to allow adding multiple students
+
     while True:
-        # Create a dictionary to store student information
-        student = {}
-        # Ask for student information
-
-        # Loop to validate the student's name and section
-        while True:
-            # Get the full name of the student (with EOFError handling)
-            try:
-                full_name = input("Ingrese el nombre completo del estudiante: ")
-            except EOFError:
-                print("\nEntrada cancelada por el usuario.\n") 
-                return # Exit the function if the user cancels the input and return to the main menu
-            # Calling the function to validate the name
-            if not is_valid_name(full_name): 
-                print("\nNombre inválido. No debe estar vacío ni contener números.\n")
-                continue
-
-            # Get the section of the student
-            section = input("Ingrese la sección del estudiante (ejemplo 10A): ")
-            # Calling the function to validate the section
-            if not is_valid_section(section): 
-                print("\nSección inválida. Ejemplo válido: 10A, 11B.\n")
-                continue
-
-            # Calling the function to check if the student already exists
-            if student_exists(students, full_name, section): 
-                print("\nEl estudiante ya existe en esa sección.\n")
-                continue
-
-            # If all validations pass, save the student's name and section
-            student["full_name"] = full_name.strip()
-            student["section"] = section.upper()
-            break
-        
-        # Get the grades for each subject and validate them
-        subjects = [
-            ("spanish_grade", "Español"),
-            ("english_grade", "Inglés"),
-            ("social_studies_grade", "Sociales"),
-            ("science_grade", "Ciencias")
-        ]
-        # Loop through each subject to get and validate the grade
-        for key, subject_name in subjects:
-            while True:
-                grade = input(f"Ingrese la nota de {subject_name}: ")
-
-                if is_valid_grade(grade):
-                    student[key] = float(grade)
-                    break # Exit the loop if the grade is valid
-                else:
-                    print("\nNota inválida. Debe ser un número entre 0 y 100.\n")
-
-        # Calculate average grade
-        student["average"] = (
-            student["spanish_grade"]
-            + student["english_grade"]
-            + student["social_studies_grade"]
-            + student["science_grade"]
-        ) / 4
-
-        # Save student to the list
+        # Get basic student information from user input and validate it
+        basic_info = get_basic_student_info(students)
+        if basic_info is None:
+            return
+        # Get student grades from user input and validate them
+        grades = get_student_grades()
+        student = {**basic_info, **grades}
+        # Calculate the average grade for the student and add it to the student dictionary
+        student["average"] = calculate_average(student)
+        # Add the student to the students list
         students.append(student)
 
         print("\nEstudiante registrado correctamente.\n")
 
-        # Ask if the user wants to add another student
-        option = input("¿Desea agregar otro estudiante? (s/n): ").lower()
-
+        option = input("\n¿Desea agregar otro estudiante? (s/n): \n").lower()
         if option not in ("s", "si", "sí"):
-            # Return to main menu
             break
 # End of function to fill student information
 
 
 # ========== Function to show students information ========== 
 def menu_2_view_all_students(students):
-    if not students: # Check if there are no students registered
-        print("\nNo hay estudiantes registrados actualmente.\n")
-        return # Exit the function if there are no students to show
-
-    print("\nListado de estudiantes registrados:\n")
-
-    # Loop through the list of students and print their information
-    for index, student in enumerate(students, start=1):
-        print(f"Estudiante #{index}")
-        print(f"Nombre Completo : {student['full_name']}")
-        print(f"Sección         : {student['section']}")
-        print(f"Nota Español    : {student['spanish_grade']}")
-        print(f"Nota Inglés     : {student['english_grade']}")
-        print(f"Nota Sociales   : {student['social_studies_grade']}")
-        print(f"Nota Ciencias   : {student['science_grade']}")
-        print("-" * 40)
-
-    print()# Print an empty line for better formatting after listing all students
+    # Call the function to get the formatted list of all students and store it in a variable(result)
+    result = get_all_students_formatted(students)
+    print(result)
 # End of function to show students information
 
 
 # ========== Function to show the Top 3 average grades ========== 
 def menu_3_top_3_students(students):
-    # Check if there are no students registered
-    if not students:
-        print("\nNo hay estudiantes registrados para mostrar.\n")
-        return
-
-    # Get ordered students from actions
-    ordered_students = sort_students_by_average(students)
-
-    print("\n===== TOP 3 ESTUDIANTES =====")
-    # Loop through the top 3 students and print their information
-    for index, student in enumerate(ordered_students[:3], start=1):
-        print(f"\n#{index}")
-        print(f"Nombre: {student['full_name']}")
-        print(f"Sección: {student['section']}")
-        print(f"Promedio: {student['average']:.2f}")
+    # Call the function to get the formatted list of the Top 3 students with the best average grade and store it in a variable(result)
+    result = get_top_3_students_formatted(students)
+    print(result)
 # End of function to show the Top 3 average grades
 
 
 # ========== Function to show the average grade among all students' grades ==========
 def menu_4_average_grade(students):
     # Check if there are no students registered
-    if not students: 
-        print("\nNo hay estudiantes registrados para calcular el promedio.\n")
+    if not students:
+        print("\nNo hay estudiantes registrados.\n")
         return
-    # Calculate the average of all students' averages
-    total_average = sum(student["average"] for student in students)
-    average_grade = total_average / len(students)
-    print(f"\nEl promedio general entre todos los estudiantes es: {average_grade:.2f}")
+    # Call the function to calculate the general average grade for all students and store it in a variable(average_grade)
+    average_grade = calculate_general_average(students)
+    # Print the average grade with two decimal places
+    print(f"\nEl promedio general es: {average_grade:.2f}")
 # End of function to show the average grade among all students' grades
 
 
@@ -155,7 +74,7 @@ def menu_5_export_data_to_csv(students):
 
 # ========== Function to import data from CSV ==========
 def menu_6_import_data_from_csv(students): 
-    #   Call the function to import students from CSV and store the result in a variable
+    #  Call the function to import students from CSV and store the result in a variable
     imported_students = imported_students_from_csv() 
 
     # Check if the imported students data is None (which indicates an error or that the file does not exist)
@@ -173,59 +92,40 @@ def menu_6_import_data_from_csv(students):
 
 # ========== Function to delete a student (to be implemented) ==========
 def menu_7_delete_student(students):
-    # Check if there are no students registered
+    # Check if there are no students registered before attempting to delete
     if not students:
         print("\nNo hay estudiantes registrados para eliminar.\n")
         return
-    # Ask the user for the full name and section of the student to delete
-    full_name = input("Ingrese el nombre completo del estudiante a eliminar: ").strip()
-    section = input("Ingrese la sección del estudiante (ejemplo 10A): ").strip()
-    # Validate the name and section inputs
-    if not student_exists(students, full_name, section):
+
+    full_name = input("Ingrese el nombre completo: ").strip()
+    section = input("Ingrese la sección: ").strip()
+    # Call the function to get the student based on the full name and section
+    student = get_student(students, full_name, section)
+
+    if not student:
         print("\nEl estudiante no existe.\n")
         return
-    # Get the student information to show it before confirming deletion
-    student = get_student(students, full_name, section)
 
     print("\nEstudiante encontrado:")
     print(f"Nombre: {student['full_name']}")
     print(f"Sección: {student['section']}")
-
     # Ask for confirmation before deleting the student
-    confirm = input("¿Está seguro de que desea eliminar este estudiante? (s/n): ").lower()
-    # If the user does not confirm, cancel the deletion and return to the main menu
+    confirm = input("¿Seguro que desea eliminar? (s/n): ").lower()
+    # If the user does not confirm with "s", "si", or "sí", cancel the deletion and return to the menu
     if confirm not in ("s", "si", "sí"):
         print("\nEliminación cancelada.\n")
         return
-    # If the user confirms, call the function to delete the student and print the result
+    # Call the function to delete the student and check if it was successful
     if delete_student(students, full_name, section):
-        print("\nEstudiante eliminado correctamente\n")
+        print("\nEstudiante eliminado correctamente.\n")
 # End of function to delete a student
 
 
 # ========== Function to show failed students (to be implemented) ==========
 def menu_8_show_failed_students(students):
-    # Check if there are no students registered
-    if not students:
-        print("\nNo hay estudiantes registrados.\n")
-        return
-    # Call the function to get the list of failed students and store it in a variable
-    failed_students = get_failed_students(students)
-    # Check if there are no failed students and print a message if that's the case, then return to the main menu
-    if not failed_students:
-        print("\nNo hay estudiantes reprobados\n")
-        return
-
-    print("\n===== ESTUDIANTES REPROBADOS =====")
-    # Loop through the list of failed students and print their information along with the subjects they failed
-    for index, student in enumerate(failed_students, start=1):
-        print(f"\n#{index}")
-        print(f"Nombre: {student['full_name']}")
-        print(f"Sección: {student['section']}")
-        print("Materias reprobadas:")
-        # Loop through the failed subjects of the student and print the subject name and grade
-        for subject, grade in student["failed_subjects"].items():
-            print(f" - {subject}: {grade}")
+    # Call the function to get the formatted list of failed students and store it in a variable
+    result = get_failed_students_formatted(students)
+    print(result)
 # End of function to show failed students
 
 
@@ -242,7 +142,7 @@ def menu(students):
         print("6. Importar los datos")
         print("7. Eliminar estudiante")
         print("8. Mostrar estudiantes reprobados")
-        print("9. Salir")
+        print("9. Salir\n")
 
         # Ask the user to select an option
         option  = input("Seleccione una opción: ")
@@ -276,4 +176,4 @@ def menu(students):
             break
         else:
             # Invalid option handling
-            print("Opción inválida. Intente de nuevo.")
+            print("\nOpción inválida. Intente de nuevo.\n")
